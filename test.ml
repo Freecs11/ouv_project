ee21f04058c727485bab88cfcb0fcf7c78696414
Freecs11 <bouhmad03@gmail.com>
2023-10-14 13:35:37 +0200

    start of Part 1

--- test.ml
+++ test.ml
@@ -1,33 +1,45 @@
-let rec for_all c l = match l with 
-  | [] -> true 
-  | x :: y -> c x && for_all c y ;;
+open Int64
 
-let rec map2  c l t = match (l , t) with 
-  | ([] , p) -> []
-  | (r , []) -> []
-  | (x::y , d::f) -> c x d :: (map2 c y f)
+type int64list = Empty | Cons of int64 * int64list
 
-let rec combine c t = 
-  match (c ,t) with 
-  | (x, [] ) -> [] 
-  |( [] , x ) -> []
-  | ( x::y , k::l) -> (x,k) :: combine y l;;
+let rec insertInt64List x l = 
+  match l with
+  | Empty -> Cons(x, Empty)
+  | Cons(h, t) -> Cons(h, insertInt64List x t)
+;;
 
+let rec removeInt64List l =
+  match l with 
+  | Empty -> failwith "error on remove"
+  | Cons(h, t) -> t
+;;
 
-(* tests with print  of previous methods *)
+let rec headInt64List l =
+  match l with 
+  | Empty -> failwith "error on head"
+  | Cons(h, t) -> h
+;;
 
-let rec map f l = match l with
-  | [] -> []
-  | x :: xs -> f x :: map f xs ;;
+let rec printInt64List l =
+  match l with
+  | Empty -> ()
+  | Cons(h, t) -> print_string (Int64.to_string h); print_string " "; printInt64List t
+;;
 
-  
-let print_int_list l = 
-  let rec aux l = match l with 
-    | [] -> ()
-    | x :: y -> print_int x ; print_string " " ; aux y 
-  in 
-  print_string "[ " ; aux l ; print_string "]\n" ;;
-  
+(*construct ne marche pas *)
+let rec constructInt64List x =
+  let rec construct x acc =
+    if x = 0L then acc
+    else
+      let c = Int64.shift_left 1L 64 in
+      let k = Int64.rem x c in
+      let l = Int64.div x c in
+      construct l (insertInt64List k acc)
+  in
+  construct x Empty
+;;
 
-  print_int_list (map (fun x -> x + 1) [1;2;3;4;5;6;7;8;9;10]) ;;
-
+let x = Int64.shift_left 1L 100 in (* 2^100 *)
+let lst = constructInt64List x in
+printInt64List lst;
+print_string "d\n";
--- test.mli
+++
@@ -1,5 +0,0 @@
-val for_all : ('a -> bool) -> 'a list -> bool
-val map2 : ('a -> 'b -> 'c) -> 'a list -> 'b list -> 'c list
-val combine : 'a list -> 'b list -> ('a * 'b) list
-val map : ('a -> 'b) -> 'a list -> 'b list
-val print_int_list : int list -> unit
